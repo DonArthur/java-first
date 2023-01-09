236c6f88a595004e387e5b7fda46b47b26e5e8b0
DonArthur <geraldy.miero@gmail.com>
2023-01-09 23:24:26 +0700

    learn about primitive data types

--- src/com/willofd/Main.java
+++ src/com/willofd/Main.java
@@ -2,6 +2,10 @@ package com.willofd;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte age = 30; // can save up to 127
+        int viewsCount = 3_123_456; // can separate numbers with underscore for readability
+        long readsCount = 1_234_567_8901L; // if it's a big number, add L or l, preferably L for readability
+        var name = "Jane";
+        System.out.println(name + " is " + age);
     }
 }
