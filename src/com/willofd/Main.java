c3967a69c8265929d624f14f56a326e76ed84b81
DonArthur <geraldy.miero@gmail.com>
2023-01-10 22:28:52 +0700

    Change the data type

--- src/com/willofd/Main.java
+++ src/com/willofd/Main.java
@@ -5,7 +5,7 @@ public class Main {
         byte age = 30; // can save up to 127
         int viewsCount = 3_123_456; // can separate numbers with underscore for readability
         long readsCount = 1_234_567_8901L; // if it's a big number, add L or l, preferably L for readability
-        var name = "Jane";
+        String name = "Jane";
         System.out.println(name + " is " + age);
     }
 }
