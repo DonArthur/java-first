63bbc1936c65872affa54ac577289c7f9e5d8929
DonArthur <geraldy.miero@gmail.com>
2023-01-10 09:14:19 +0700

    input and output

--- src/com/willofd/Main.java
+++ src/com/willofd/Main.java
@@ -1,11 +1,13 @@
 package com.willofd;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        byte age = 30; // can save up to 127
-        int viewsCount = 3_123_456; // can separate numbers with underscore for readability
-        long readsCount = 1_234_567_8901L; // if it's a big number, add L or l, preferably L for readability
-        var name = "Jane";
-        System.out.println(name + " is " + age);
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter a task");
+
+        String task = myObj.nextLine();
+        System.out.println("Your new task is " + task);
     }
 }
